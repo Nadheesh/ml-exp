a627af0958e72d9fbf79e5ccd2f6b58bdb66ff98
nadheesh <nadheeshj@gmail.com>
2018-05-24 11:10:15 +0530

    Adding error bar plots for prediction uncertainty

---
+++ visualizing/predictive_uncertaity.py
@@ -0,0 +1,36 @@
+"""
+Copyright 2018 Nadheesh Jihan
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def plot_predictive_uncertainty(X, true_y, prediction, error):
+    """
+    plots the prediction and their uncertainty/error/variance using error bars
+
+    *** uses the log of the error to improve the visibility ***
+    :param X:
+    :param prediction:
+    :param error:
+    :return:
+    """
+    fig = plt.figure()
+    plt.scatter(np.array(X).T[0], true_y, label='true', c='r')
+    plt.errorbar(np.array(X).T[0], prediction, yerr=np.log(error), fmt='o', label='prediction', c='y')
+    plt.legend(loc=0)
+
+    return fig.axes
