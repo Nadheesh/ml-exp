089220be254e86eb344e21f1eba0b4bf93fcd433
nadheesh <nadheeshj@gmail.com>
2018-05-24 12:04:17 +0530

    Removing test scripts

--- bayesian/model_api.py
+++ bayesian/model_api.py
@@ -15,14 +15,12 @@ Copyright 2018 Nadheesh Jihan
 """
 
 import numpy as np
-import pandas as pd
 import pymc3 as pm
 from theano import shared, tensor as tt
 
-from data.data_generation import generate_data
-
 seed = 7
 PPC_CONST = 20000
+
 
 def _adding_intecept(X):
     """
@@ -46,7 +44,7 @@ def _init_model(X, y):
         alpha = pm.Gamma("alpha", alpha=1e-2, beta=1e-4)
 
         # Define priors'
-        w = pm.Normal("w", mu=0, sd=alpha, shape=train_X.shape[1])
+        w = pm.Normal("w", mu=0, sd=alpha, shape=X.get_value().shape[1])
         sigma = pm.HalfCauchy("sigma", beta=10)
         mu = tt.dot(w, X.T)
 
@@ -59,9 +57,9 @@ class Model:
 
     def __init__(self, n_draws, init_model=None):
         """
-        initiate the pymc3 model
+        initiate the bayesian model
         :param n_draws: number of samples drawn from the target distributions
-        :param init_model: pymc3 model initiation, check example provided
+        :param init_model: bayesian model initiation, check example provided
         """
         self.n_draws = n_draws
 
@@ -83,7 +81,7 @@ class Model:
         with self.model:
             self.trace = pm.sample(self.n_draws, random_seed=seed, njobs=1)
 
-    def predict(self, X, with_error = False):
+    def predict(self, X, with_error=False):
         """
         predict using the train model
         :param X:
@@ -98,33 +96,6 @@ class Model:
 
         self.shared_X.set_value(train_X)
 
-        if with_error :
+        if with_error:
             return ppc['y'].mean(axis=0), ppc['y'].std(axis=0)  # return prediction and error
         return ppc['y'].mean(axis=0)
-
-
-# HOW TO USE THE MODEL
-if __name__ == '__main__':
-
-    n = 500
-    d = 5
-
-    # use other scripts
-    train_X, test_X, train_y, test_y = generate_data(n, d, 0.2)
-
-    train_X, test_X = _adding_intecept(train_X), _adding_intecept(test_X)
-
-    model = Model(n_draws=1000, init_model=_init_model)
-    model.fit(train_X, train_y)
-
-    pred, err = model.predict(test_X, with_error=True)
-
-    from matplotlib import pyplot as plt
-    pm.traceplot(model.trace)
-
-    from visualizing.predictive_uncertaity import plot_predictive_uncertainty
-    plot_predictive_uncertainty(test_X, test_y, pred, err)
-
-    plt.show()
-
-
